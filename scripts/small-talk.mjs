e5de8e6c5af111ab8649461533431c8248dc2725
alakshana <etiquettestartshere@gmail.com>
2024-02-28 23:32:20 -0500

    buncha changes

--- scripts/small-talk.mjs
+++ scripts/small-talk.mjs
@@ -7,35 +7,53 @@ export class smallTalk {
     smallTalk._hidePortrait();
     smallTalk._hideSubtitle();
     smallTalk._hideItemImg();
+    smallTalk._portraitAndSubtitle();
     Hooks.on("dnd5e.renderChatMessage", smallTalk._GM);
   };
 
   static _blueChecks() {
     const blue = game.settings.get(MODULE, "blueChecks");
-    const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/blue-hit-check.css" />`
-    if (blue) document.head.insertAdjacentHTML('beforeend', link);
+    if (blue) {
+      const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/blue-hit-check.css" />`
+      document.head.insertAdjacentHTML('beforeend', link);
+    };  
   };
 
   static _hidePortrait() {
     const hideArt = game.settings.get(MODULE, "hidePortrait");
-    const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/hide-portrait.css" />`
-    if (hideArt) document.head.insertAdjacentHTML('beforeend', link);
+    if (hideArt) {
+      const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/hide-portrait.css" />`
+      document.head.insertAdjacentHTML('beforeend', link);
+    };
   };
 
   static _hideSubtitle() {
-    const hideArt = game.settings.get(MODULE, "hideSubtitle");
-    const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/hide-subtitle.css" />`
-    if (hideArt) document.head.insertAdjacentHTML('beforeend', link);
+    const hideSub = game.settings.get(MODULE, "hideSubtitle");
+    if (hideSub) {
+      const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/hide-subtitle.css" />`
+      document.head.insertAdjacentHTML('beforeend', link);
+    };  
   };
 
   static _hideItemImg() {
     const hideArt = game.settings.get(MODULE, "hideItemImg");
-    const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/hide-item-img.css" />`
-    if (hideArt) document.head.insertAdjacentHTML('beforeend', link);
+    if (hideArt) {
+      const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/hide-item-img.css" />`
+      document.head.insertAdjacentHTML('beforeend', link);
+    };  
+  };
+
+  static _portraitAndSubtitle() {
+    const hideArt = game.settings.get(MODULE, "hidePortrait");
+    const hideSub = game.settings.get(MODULE, "hideSubtitle");
+    if (hideArt && hideSub) {
+      const link = `<link rel="stylesheet" href="/modules/smalltalkdnd5e/styles/portrait-and-subtitle.css" />`
+      document.head.insertAdjacentHTML('beforeend', link);
+    };  
   };
 
   static _GM(message, html) {
-    if (!message.speaker.actor && !message.speaker.token) {
+    if ((!message.speaker.actor && !message.speaker.token) || (!game.settings.get(MODULE, "hidePortrait") && game.settings.get(MODULE, "hideSubtitle"))) {
       const subtitle = html.querySelector('.message-header .subtitle');
       subtitle.textContent = String.fromCharCode(8203);
     };  
